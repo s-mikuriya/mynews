d30e4639334a40d8d587e193c88d454b3ac3d1f5
Mikuriya <bad.shin3196@icloud.com>
2020-03-19 09:23:57 +0000

    PHP_09

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+//PHP_09
+//課題３
+Route::get('XXX', 'Admin\AAAController@bbb');
+
+//課題4
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
