1be04d9790a172d8ba02efab7081f36dd8283918
Mikuriya <bad.shin3196@icloud.com>
2020-03-24 05:41:05 +0000

    PHP_12

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,20 @@
         <title>create.blade.php</title>
     </head>
     <body>
-        <h1>create.blade.php</h1>
+        <!-- <h1>create.blade.php</h1> -->
+        
+        <!-- PHP_11 課題4 -->
+        @extends('layouts.profile')
+        @section('title', 'プロフィール')
+        
+        @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィール</h2>
+                </div>
+            </div>
+        </div>
+        @endsection
     </body>
 </html>
