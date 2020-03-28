1e9dad6b992250d3ad9a1c4452d1e3e68272d36e
Mikuriya <bad.shin3196@icloud.com>
2020-03-28 03:50:17 +0000

    PHP_12

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -39,7 +39,29 @@
                         </ul>
                         <!-- Right Side Of Navbar -->
                         <ul class ="navbar-nav ml auto">
+                            <!-- PHP_12 課題4 -->
+                            <!-- Authenication Links -->
+                            @guest
+                            <li class="nav-link" href="{{ route('login')}}">{{__('login')}}</a></li>
                             
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{Auth::user()->name }}<span class="caret"></span>
+                                </a>
+                                
+                                <div class="dropdown-menu" area-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault()
+                                    document.getElementById('logout-form).submit();">
+                                        {{__('logout')}}
+                                    </a>
+                                    
+                                    <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
                         </ul>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 //PHP_09
@@ -25,6 +25,12 @@ Route::get('XXX', 'Admin\AAAController@bbb');
 
 //課題4
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    //PHP_12 課題2
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    //PHP_12 課題3
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
