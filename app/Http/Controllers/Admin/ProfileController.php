5f262cad3d57b4311bfbc2c22246c8443ba76e7a
Mikuriya <bad.shin3196@icloud.com>
2020-04-04 07:33:39 +0000

    PHP_16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+//PHP_14 課題5 以下でProfile Modelが扱えるようになる
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -12,18 +15,63 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        //PHP_14 課題5
+        //Validationを行う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        //フォームから送信されてきた_takenを削除する
+        unset($form['taken']);
+        
+        //データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
+        //admin/profile/createにリダイレクトする
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            //検索されたら検索結果を取得する
+            $posts = Profile::where('name', $cond_name)->get();
+        } else {
+            //それ以外は全てのプロフィールを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['pposts' => $posts, 'cond_name' => $cond_name]);
     }
     
-    public function update()
+    public function edit(Request $request)
     {
-        return redirect('admin/profile/edit');
+        //Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+    
+    public function update(Request $request)
+    {
+        //Validationをかける
+        $this->validate($request, Profile::$rules);
+        //Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        //送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        
+        unset($profile_form['_token']);
+        
+        //該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile');
     }
 }
